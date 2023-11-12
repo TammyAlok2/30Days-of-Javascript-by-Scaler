90cc3113c219108e36bcef7e2705342c6f046f18
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-11-13 00:01:04 +0530

    Day 7 completed

---
+++ Day-5/solutuioon.js
@@ -0,0 +1,29 @@
+// program to get the file extension
+
+function getFileExtension(filename){
+
+    // get file extension
+    const extension = filename.split('.').pop();
+    return extension;
+}
+
+// passing the filename
+const result1 = getFileExtension('module.js');
+console.log(result1);
+
+const result2 = getFileExtension('module.txt');
+console.log(result2);// program to get the file extension
+
+function getFileExtension(filename){
+
+    // get file extension
+    const extension = filename.split('.').pop();
+    return extension;
+}
+
+// passing the filename
+const result1 = getFileExtension('module.js');
+console.log(result1);
+
+const result2 = getFileExtension('module.txt');
+console.log(result2);
---
+++ Day-7/solution.js
@@ -0,0 +1,10 @@
+/*
+Write a JavaScript program to check a pair of numbers and return true if one of the numbers is 50 or if their sum is 50.
+
+*/
+let a =10;
+let b=40;
+if (a==50 || a+b==50)
+console.log('true');
+else 
+console.log('false')
