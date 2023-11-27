c2c8f7628a1d0d7c950be3025997c2dab730d411
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-11-27 18:19:17 +0530

    day 22  completed successfully

---
+++ Day-22/solution.js
@@ -0,0 +1,14 @@
+function compareObjects(obj1, obj2) {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+        return false
+    }
+    for (let key in obj1) {
+        if (typeof obj1[key] !== 'object' && typeof obj2[key] !== 'object') {
+            if (obj1[key] !== obj2[key]) {
+                return false
+            }
+        }
+        else return compareObjects(obj1[key], obj2[key])
+    }
+    return true
+}
